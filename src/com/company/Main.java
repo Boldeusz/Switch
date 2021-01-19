f733b1e1458cd42b323fce24d56a2ab1e1c0e609
Boldeusz <mnpower@poczta.onet.pl>
2021-01-19 18:08:47 +0100

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int value = 2;
+//        if (value == 1){
+//            System.out.println("Value was 1");
+//        }else if (value == 2){
+//            System.out.println("Value was 2");
+//        }else {
+//            System.out.println("Value was not 1 or 2");
+//        }
+
+        int switchValue = 1;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.println("Value was not 1 or 2");
+                break;
+        }
+        char value = 'A';
+        switch (value){
+            case 'A':case 'B':case 'C':case 'D':case 'E':
+                System.out.println("Was A, B, C or D");
+                System.out.println("Character found " +value);
+                break;
+            default:
+                System.out.println("Non of the characters found");
+                break;
+        }
+    }
+}
